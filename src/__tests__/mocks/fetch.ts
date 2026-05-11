80d95acd45e3c827f6fb287349836ceb9f783d6e
Jahongir <nusratovjahongir@gmail.com>
2026-05-11 11:49:47 +0500

    feat: setup vitest and rtl

---
+++ src/__tests__/mocks/fetch.ts
@@ -0,0 +1,8 @@
+// export function mockFetch(data: any, ok = true) {
+//   global.fetch = vi.fn(() =>
+//     Promise.resolve({
+//       ok,
+//       json: () => Promise.resolve(data),
+//     } as Response),
+//   );
+// }
